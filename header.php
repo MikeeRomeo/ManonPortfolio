850212b4ad08229749c3ea06362b79cd8a105895
Mike <mikeeromeo@gmail.com>
2021-09-20 17:28:45 +0200

    mobile nav progress

--- header.php
+++ header.php
@@ -11,28 +11,39 @@
     <header id="header" role="header">
         <div class="container">
             <div class="container__row">
-
                 <?php include 'theme-logo.php'; ?>
-                <nav id="menu" class="nav" role="navigation" itemscope
-                     itemtype="https://schema.org/SiteNavigationElement">
-<!--                    <button type="button" class="menu-toggle">-->
-<!--                        <span class="menu-icon">&#9776;</span>-->
-<!--                        <span class="menu-text screen-reader-text">-->
-<!--                    --><?php //esc_html_e(' Menu', 'generic'); ?><!--</span>-->
-<!--                    </button>-->
-                    <?php
-                    wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">',
-                        'link_after' => '</span>'));
-                    ?>
-                </nav>
 
-                <?php
-                wp_nav_menu( array(
-                    'theme_location' => 'socials-menu',
-                    'container_class' => 'socials-nav nav' ) );
-                ?>
+<!--                <nav id="menu" class="nav" role="navigation" itemscope-->
+<!--                     itemtype="https://schema.org/SiteNavigationElement">-->
+<!--                    --><?php
+//                    wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">',
+//                        'link_after' => '</span>'));
+//                    ?>
+<!--                </nav>-->
+<!---->
+<!--                --><?php
+//                wp_nav_menu( array(
+//                    'theme_location' => 'socials-menu',
+//                    'container_class' => 'socials-nav nav' ) );
+//                ?>
 
+                <div class="nav--wrapper">
+                    <?php include 'nav-main.php'; ?>
+                </div>
+
+                <div class="nav-mobile-button">
+                    <img class="open-icon"
+                         src="<?php echo get_template_directory_uri(); ?>/assets/icons/menu.png"
+                         alt="Mobiele naviagtie knop">
+                    <img class="close-icon"
+                         src="<?php echo get_template_directory_uri(); ?>/assets/icons/cancel.png"
+                         alt="Mobiele naviagtie knop">
+                </div>
+                
             </div>
+
         </div>
     </header>
+
+
     <div id="container">
---
+++ nav-main.php
@@ -0,0 +1,13 @@
+<nav id="menu" class="nav" role="navigation" itemscope
+     itemtype="https://schema.org/SiteNavigationElement">
+    <?php
+    wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">',
+        'link_after' => '</span>'));
+    ?>
+</nav>
+
+<?php
+wp_nav_menu( array(
+    'theme_location' => 'socials-menu',
+    'container_class' => 'socials-nav nav' ) );
+?>
